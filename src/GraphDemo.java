44662ca6349c59bf1979b6054c158c7c894f0171
Daniel Duarte <daniel.duarte@duke.edu>
2022-12-06 17:38:57 -0500

    GraphProcessor completed

--- src/GraphDemo.java
+++ src/GraphDemo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Demonstrates the calculation of shortest paths in the US Highway
  * network, showing the functionality of GraphProcessor and using
@@ -6,6 +8,19 @@
  */
 public class GraphDemo {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the city you want to start with:");
+        String firstCity = s.nextLine();
+        System.out.println("Enter the city you want to go to:");
+        String secondCity = s.nextLine();
+        Point a = nearestPoint(new Point(0, 0));
+        Point b = nearestPoint(new Point(0, 0));
 
+
+
+
+
+
+      
     }
 }
--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,5 +1,14 @@
 import java.security.InvalidAlgorithmParameterException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
 import java.io.FileInputStream;
 
 /**
@@ -10,6 +19,8 @@ import java.io.FileInputStream;
  *
  */
 public class GraphProcessor {
+    private HashMap<Point, List<Point>> vertices;
+
     /**
      * Creates and initializes a graph from a source data
      * file in the .graph format. Should be called
@@ -18,7 +29,41 @@ public class GraphProcessor {
      * @throws Exception if file not found or error reading
      */
     public void initialize(FileInputStream file) throws Exception {
-        // TODO: Implement initialize
+      vertices = new HashMap<>();
+      Scanner reader = new Scanner(file);
+      int numVertices = reader.nextInt();
+      int numEdges = reader.nextInt();
+      ArrayList<Point> pts = new ArrayList<>();
+
+      reader.nextLine();
+
+      // Read next "numVertices" lines
+      for (int i = 0; i < numVertices; i++) {
+        String nextLine = reader.nextLine();
+        String[] elements = nextLine.split(" ");
+        double latitude = Double.parseDouble(elements[1]);
+        double longtidue = Double.parseDouble(elements[2]);
+        Point np = new Point(latitude, longtidue);
+
+        vertices.putIfAbsent(np, new ArrayList<>());
+        pts.add(np);
+      }
+
+      // Read next "numEdges" lines
+      for (int i = 0; i < numEdges; i++) {
+        String nextt = reader.nextLine();
+        String[] arr = nextt.split(" ");
+        int startIndex = Integer.parseInt(arr[0]);
+        int endIndex = Integer.parseInt(arr[1]);
+
+        Point startingPt = pts.get(startIndex);
+        Point endingPt = pts.get(endIndex);
+
+        vertices.get(startingPt).add(endingPt);
+        vertices.get(endingPt).add(startingPt);
+      }
+
+      reader.close();
     }
 
 
@@ -29,8 +74,17 @@ public class GraphProcessor {
      * @return The closest point in the graph to p
      */
     public Point nearestPoint(Point p) {
-        // TODO: Implement nearestPoint
-        return null;
+        Point closest = null;
+        double shortestDistance = 999999999;
+
+        for (Point np : vertices.keySet()) {
+          if (np.distance(p) < shortestDistance) {
+            closest = np;
+            shortestDistance = np.distance(p);
+          }
+        }
+
+        return closest;
     }
 
 
@@ -44,8 +98,12 @@ public class GraphProcessor {
      * @return The distance to get from start to end
      */
     public double routeDistance(List<Point> route) {
+        double distance = 0.0;
+        for (int i = 0; i<route.size()-1; i++) {
+            distance += route.get(i).distance(route.get(i+1));
+        }
+        return distance;
         // TODO Implement routeDistance
-        return 0.0;
     }
     
 
@@ -58,10 +116,33 @@ public class GraphProcessor {
      * @return true if p2 is reachable from p1 (and vice versa)
      */
     public boolean connected(Point p1, Point p2) {
-        // TODO: Implement connected
+        if (vertices.get(p1).contains(p2))
+            return true;
+        if (!vertices.keySet().contains(p1) || !vertices.keySet().contains(p2))
+            return false;
+        
+        Set<Point> visited = new HashSet<>();
+        Map<Point, Point> previous = new HashMap<>();
+        Stack<Point> toExplore = new Stack();
+        Point current = p1;
+        toExplore.add(current);
+        visited.add(current);
+
+        while(!toExplore.isEmpty()) {
+          current = toExplore.pop();
+          for (Point neighbor : vertices.get(current)) {
+            if (neighbor.equals(p2))
+              return true;
+            if (!visited.contains(neighbor)) {
+              previous.put(neighbor, current);
+              visited.add(neighbor);
+              toExplore.push(neighbor);
+            }
+          }
+        }
+
         return false;
     }
-
 
     /**
      * Returns the shortest path, traversing the graph, that begins at start
@@ -76,9 +157,42 @@ public class GraphProcessor {
      * either because start is not connected to end or because start equals end.
      */
     public List<Point> route(Point start, Point end) throws InvalidAlgorithmParameterException {
-        // TODO: Implement route
-        return null;
-    }
+        if (!vertices.keySet().contains(start) || !vertices.keySet().contains(end) || start.equals(end) ||
+            connected(start, end) == false) {
+            throw new InvalidAlgorithmParameterException("No path between start and end");   
+        }
 
-    
+        Map<Point, Double> distance = new HashMap<>();
+        List<Point> path = new ArrayList<>();
+        Map<Point, Point> previous = new HashMap<>();
+        PriorityQueue<Point> toExplore = new PriorityQueue<>();
+        Point curr = start;
+
+        distance.put(curr, 0.0);
+        toExplore.add(curr);
+
+        while(!toExplore.isEmpty()) {
+          curr = toExplore.remove();
+          for (Point neighbor : vertices.get(curr)) {
+            double weight = curr.distance(neighbor);
+            if (!distance.containsKey(neighbor) || distance.get(neighbor) > distance.get(curr) + weight) {
+              distance.put(neighbor, distance.get(curr) + weight);
+              previous.put(neighbor, curr);
+              toExplore.add(neighbor);
+            }
+          }
+        }
+
+        curr = end;
+
+        while(!curr.equals(start)) {
+          path.add(curr);
+          curr = previous.get(curr);
+        }
+
+        path.add(start);
+        Collections.reverse(path);
+        
+        return path;
+    }
 }
