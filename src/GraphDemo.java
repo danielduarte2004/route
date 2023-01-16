efb5dccb380ac5acb678508c41f877ae7e78065e
Daniel Duarte <daniel.duarte@duke.edu>
2023-01-15 22:16:52 -0500

    README.md and .gitignore added

--- src/GraphDemo.java
+++ src/GraphDemo.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -7,20 +11,70 @@ import java.util.Scanner;
  * To do: Add your name(s) as authors
  */
 public class GraphDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the city you want to start with:");
+        
+        System.out.println();
+        System.out.println("Enter the name of the city you want to start with:");
         String firstCity = s.nextLine();
-        System.out.println("Enter the city you want to go to:");
+        
+        System.out.println();
+        System.out.println("Enter the name of the city you want to go to:");
         String secondCity = s.nextLine();
-        Point a = nearestPoint(new Point(0, 0));
-        Point b = nearestPoint(new Point(0, 0));
+        
+        GraphProcessor gp = new GraphProcessor();
+        GraphDemo gd = new GraphDemo();
+        gp.initialize(new FileInputStream(new File("data/usa.graph")));
 
+        Point firstCityCoord = gd.getPoint(firstCity);
+        Point secondCityCoord = gd.getPoint(secondCity);
+        
+        /** START TIME */
+        long startTime = System.nanoTime();
 
+        Point a = gp.nearestPoint(firstCityCoord);
+        Point b = gp.nearestPoint(secondCityCoord);
+        
+        List<Point> bestPath = gp.route(a, b);
+        double distance = gp.routeDistance(bestPath);
+        
+        long elapsedNanos = System.nanoTime() - startTime; 
+        /** END TIME */
 
+        System.out.println();
+        System.out.println("Nearest point to " + firstCity + " is " + a);
+        System.out.println("Nearest point to " + secondCity + " is " + b);
+        System.out.printf("The total length of the route is %.2f miles\n", distance);
+        System.out.printf("It took %.2f ms for the code to get nearest points, route, and get distance.\n", elapsedNanos/1000000.0);
 
+        
+        Visualize v = new Visualize("data/usa.vis", "images/usa.png");
+        v.drawPoint(a);
+        v.drawPoint(b);
 
+        v.drawRoute(bestPath);
+    }
 
+    private Point getPoint(String name) throws Exception {
+      Scanner reader;
+      reader = new Scanner(new File("data/uscities.csv"));
       
+      while (reader.hasNextLine()) {
+        try {
+          String[] info = reader.nextLine().split(",");
+          String cityName = info[0] + "," + info[1];
+          if(!name.equals(cityName))
+            continue;
+          double lat = Double.parseDouble(info[2]);
+          double longitude = Double.parseDouble(info[3]);
+          
+          reader.close();
+          return new Point(lat, longitude);
+        } catch(Exception e) {
+          continue;    
+        }
+      }
+
+      return null;
     }
 }
